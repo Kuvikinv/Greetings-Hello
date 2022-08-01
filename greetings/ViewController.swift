5866bca8d74049f20f28b1328634179989d51e78
Kuvikinv <kuvikinvova@gmail.com>
2022-08-01 18:27:58 +0700

    Create button

--- greetings/ViewController.swift
+++ greetings/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        greetingLabel.isHidden = true
+        greetingButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func greetingButtonPressed() {
+        greetingLabel.isHidden.toggle()
+        
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Show greeting" : "Hide greeting" ,
+            for: .normal
+        )
+//        if greetingLabel.isHidden {
+//            greetingButton.setTitle("Show greeting", for: .normal)
+//        } else {
+//
+//        }
+    }
+    
 }
 
